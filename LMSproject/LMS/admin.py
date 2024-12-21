1edb86c4c0d567929a85cac4cf1e530b92c15be3
LavanyaBansod <125907369+LavanyaBansod@users.noreply.github.com>
2024-12-21 20:43:27 +0530

    Delete LMSproject directory

--- LMSproject/LMS/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Member, Book, Copy, IssuedBook
-
-admin.site.register(Member)
-admin.site.register(Book)
-admin.site.register(Copy)
-admin.site.register(IssuedBook)
